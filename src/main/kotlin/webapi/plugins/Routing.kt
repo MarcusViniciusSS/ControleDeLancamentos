6e84ec77dc9a34b720247821876efcc2113c201c
Marcus.Silva <marcus.silva@xpi.com.br>
2023-09-21 17:20:08 -0300

    feature: add method delete.

--- src/main/kotlin/webapi/plugins/Routing.kt
+++ src/main/kotlin/webapi/plugins/Routing.kt
@@ -1,21 +1,22 @@
 package webapi.plugins
 
-import application.usecases.AdicionarLancamentos
 import application.usecases.BuscarCaixa
 import application.usecases.CriarCaixa
 import domain.aggregates.Caixa
+import domain.interfaces.ICaixaRepository
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
-import kotlin.reflect.typeOf
 
 fun Application.configureRouting() {
 
     val criarCaixa by inject<CriarCaixa>()
     val buscarCaixa by inject<BuscarCaixa>()
+    val repository by inject<ICaixaRepository>()
 
     routing {
         get ("caixa/{id}") {
@@ -28,17 +29,21 @@ fun Application.configureRouting() {
         }
 
         post ("caixa") {
-
+            val caixa = call.receive<Caixa>()
         }
 
         put ("caixa/{id}") {
-//            val id = call.parameters["id"]
-//            //val caixa = buscarCaixa.execute(id)/
-//            if (caixa == null) {
-//                call.respond(404)
-//            }
-//
-//            call.respond(caixa ?: NullBody)
+
+        }
+
+        delete ("caixa/{id}") {
+            val id : Int? = call.parameters["id"]?.toInt()
+            val resultado = id?.let { repository.Deletar(it) }
+
+            when {
+                resultado == true -> call.respond("Removido")
+                else -> call.respond(HttpStatusCode.NotFound)
+            }
         }
     }
 }
